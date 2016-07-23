e16be45ff0d54c4365e486da7e7f7cc24c4b1c95
MPrice19 <scottmakesmesick@gmail.com>
2016-07-23 21:58:06 +0000

    Initialize repository43

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'site/about'
+
+  get 'site/contact'
+
+  get 'site/cart'
+
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

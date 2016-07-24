29fd35d2b6aad73424c3e4632e8212528d2bac85
MPrice19 <scottmakesmesick@gmail.com>
2016-07-24 11:41:52 +0000

    ready2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
+  get 'cart/index'
+
   get 'site/about'
 
   get 'site/contact'
 
   get 'site/cart'
+  
+  get '/about' => 'site#about'
+  
+  get '/contact' => 'site#controller'
+  
+  get '/admin' => 'user#admin_login'
+  
+  get '/logout' => 'user#logout'
+  
+  get '/cart' => 'cart#index'
+  get '/cart'=> 'cart#clearCart'
+  
+  get '/cart/:id' => 'cart#add'
 
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
